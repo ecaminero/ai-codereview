21581951ae4b769dde86d1afe43930bd82a6dbec
Edwin <ecaminero@>
2024-04-24 00:04:16 -0400

    feat(default-comment): setting tests for default comment

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"ai-codereview/pkg/application"
 	"fmt"
+	"os"
 )
 
 func main() {
-
-	comment := application.GetRandomComments()
-	fmt.Printf("Comment: %s", comment)
+	model_retries := os.Getenv("model_retries")
+	comment, _ := application.CodeReview()
+	fmt.Printf("------------Comment: %s", comment)
+	fmt.Printf("------------Comment: %s", model_retries)
 }
--- pkg/application/create-code-revew.go
+++ pkg/application/create-code-revew.go
@@ -1,12 +1,46 @@
 package application
 
 import (
-	"math/rand"
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-github/v61/github"
+	"golang.org/x/oauth2"
 )
 
-var comments = []string{"shark", "jellyfish", "squid", "octopus", "dolphin"}
+func CodeReview() (string, error) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	// owner and repo correspond to the Github repository you want to interact with
+	owner := "ecaminero"
+	repo := os.Getenv("GITHUB_REPOSITORY")
+	ref := os.Getenv("GITHUB_REF")
+	splitRef := strings.Split(ref, "/")
+	number, err := strconv.Atoi(splitRef[2])
 
-func GetRandomComments() string {
-	i := rand.Intn(len(comments))
-	return comments[i]
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return "", err
+	}
+	body := `This is a multi-line string.
+	It can contain multiple lines.
+	Each new line is represented by a new line in the string.`
+	comment := &github.IssueComment{Body: github.String(body)}
+	comment, _, err = client.Issues.CreateComment(ctx, owner, repo, number, comment)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return "", err
+	}
+
+	fmt.Printf("Created comment at %s\n", *comment.HTMLURL)
+
+	return body, nil
 }
