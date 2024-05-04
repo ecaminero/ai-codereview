320909ecbb89479995d55fc741fb72da21bc96ed
Edwin <ecaminero@>
2024-05-04 00:36:45 -0400

    feat(print): print init data

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println("-- Start --:")
 	model_retries := os.Getenv("model_retries")
 	comment, _ := application.CodeReview()
 	fmt.Println("------------Comment:", comment)
