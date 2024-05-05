bc9bf6054f583707fbe1b245bec7e11a2b333260
Edwin <ecaminero@>
2024-05-05 00:32:09 -0400

    feat(logs): test pr pupeline

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,27 @@ import (
 
 func main() {
 	fmt.Println("-- Start --:")
-	model_retries := os.Getenv("model_retries")
+	repo := os.Getenv("GITHUB_REPOSITORY")
+	repo_owner := os.Getenv("GITHUB_REPOSITORY_OWNER")
+	eventName := os.Getenv("GITHUB_EVENT_NAME")
+	workflow := os.Getenv("GITHUB_WORKFLOW")
+	runNumber := os.Getenv("GITHUB_RUN_NUMBER")
+	actor := os.Getenv("GITHUB_ACTOR")
+	eventPath := os.Getenv("GITHUB_EVENT_PATH")
+	head_ref := os.Getenv("GITHUB_HEAD_REF")
+	ref := os.Getenv("GITHUB_REF")
+
+	// debug data
+	fmt.Printf("Repository: %s\n", repo)
+	fmt.Printf("Event Name: %s\n", eventName)
+	fmt.Printf("Workflow: %s\n", workflow)
+	fmt.Printf("Run Number: %s\n", runNumber)
+	fmt.Printf("Actor: %s\n", actor)
+	fmt.Printf("Event Path: %s\n", eventPath)
+	fmt.Printf("Repo owner: %s\n", repo_owner)
+	fmt.Printf("Head Ref: %s\n", head_ref)
+	fmt.Printf("Ref: %s\n", ref)
+
 	comment, _ := application.CodeReview()
 	fmt.Println("------------Comment:", comment)
-	fmt.Println("------------Comment:", model_retries)
 }
--- pkg/application/create-code-revew.go
+++ pkg/application/create-code-revew.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/google/go-github/v61/github"
-	"golang.org/x/oauth2"
 )
 
 type Event struct {
@@ -16,38 +16,55 @@ type Event struct {
 }
 
 func CodeReview() (string, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
+	client := github.NewClient(nil).WithAuthToken(token)
 	dateTime := time.Now().Format("2006-01-02 15:04:05")
 
 	// owner and repo correspond to the Github repository you want to interact with
-	owner := "ecaminero"
+	// Debug vars
+	owner := os.Getenv("GITHUB_REPOSITORY_OWNER")
 	repo := os.Getenv("GITHUB_REPOSITORY")
-	// Get the event from the GITHUB_EVENT_PATH
+
+	// Get the GITHUB_EVENT_PATH environment variable
 	eventPath := os.Getenv("GITHUB_EVENT_PATH")
+
+	// Read the event payload file
 	data, err := os.ReadFile(eventPath)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return "", err
 	}
-
+	// Unmarshal the event payload into an Event struct
 	var event Event
+	fmt.Println("Data: ", data)
 	err = json.Unmarshal(data, &event)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return "", err
 	}
-	number := event.Issue.GetNumber()
+
+	// Check if event.Issue and event.Issue.Title are not nil
+	if event.Issue != nil && event.Issue.Title != nil {
+		// Get the pull request title
+		title := *event.Issue.Title
+		fmt.Printf("Pull request title: %s\n", title)
+	} else {
+		fmt.Println("Issue or Issue Title is nil")
+		return "", fmt.Errorf("issue or Issue Title is nil")
+	}
+
+	number := 9
+
 	body := `This is an example comment` + dateTime
 	comment := &github.IssueComment{Body: github.String(body)}
 	createdComment, _, err := client.Issues.CreateComment(ctx, owner, repo, number, comment)
 
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		log.Fatalf("Error: %v\n", err)
 		return "", err
 	}
 	fmt.Printf("Created comment at %s\n", *createdComment.HTMLURL)
